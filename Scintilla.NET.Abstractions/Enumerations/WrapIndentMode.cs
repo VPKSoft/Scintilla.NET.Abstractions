f16fedec341b6ffea6c44a507bfca390da66f0de
VPKSoft <vpksoft@vpksoft.net>
2023-02-14 19:05:13 +0200

    Typo fix.

--- Scintilla.NET.Abstractions/Enumerations/WrapIndentMode.cs
+++ Scintilla.NET.Abstractions/Enumerations/WrapIndentMode.cs
@@ -1,23 +1,25 @@
-﻿namespace ScintillaNet.Abstractions.Enumerations;
+﻿using ScintillaNet.Abstractions.Interfaces;
+
+namespace ScintillaNet.Abstractions.Enumerations;
 
 /// <summary>
-/// Indenting behavior of wrapped sublines.
+/// Indenting behavior of wrapped sub-lines.
 /// </summary>
 public enum WrapIndentMode
 {
     /// <summary>
-    /// Wrapped sublines aligned to left of window plus the amount set by <see cref="ScintillaNET.Scintilla.WrapStartIndent" />.
+    /// Wrapped sub-lines aligned to left of window plus the amount set by <see cref="IScintillaProperties.WrapStartIndent" />.
     /// This is the default.
     /// </summary>
     Fixed,
 
     /// <summary>
-    /// Wrapped sublines are aligned to first subline indent.
+    /// Wrapped sub-lines are aligned to first sub-line indent.
     /// </summary>
     Same,
 
     /// <summary>
-    /// Wrapped sublines are aligned to first subline indent plus one more level of indentation.
+    /// Wrapped sub-lines are aligned to first sub-line indent plus one more level of indentation.
     /// </summary>
     Indent = ScintillaConstants.SC_WRAPINDENT_INDENT,
 }
